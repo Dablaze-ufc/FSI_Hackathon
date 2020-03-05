3b70afbaaf5746b2f005efd7aa9b1e5ffcc69db9
dablaze-ufc <chukwuwauchenna@gmail.com>
2020-03-05 13:06:08 +0100

    Initial commit

---
+++ app/src/main/java/com/phoenix/fsihackathon/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.phoenix.fsihackathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.fsihackathon.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
